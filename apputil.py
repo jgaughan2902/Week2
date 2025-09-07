b2af3931a91fce46ea72b4a85a852b9cb8de9316
jgaughan2902 <jdgaugha@iu.edu>
2025-09-07 21:01:30 +0000

    -

--- apputil.py
+++ apputil.py
@@ -1,12 +1,21 @@
-
 import numpy as np
 
 
 def ways(n):                 # Define the ways function with n as the input.
     return n // 5 + 1        # Return input n floor divided by 5 plus 1 because zero nickles is always an option so the answer will be
 
+import numpy as np
+
 def lowest_score(names, scores):
-    return None
+    lowScoreInd = np.argmin(scores)
+    lowScoreStu = names[lowScoreInd]
+    return str(lowScoreStu)
+
+lowest_score(names, scores)
 
 def sort_names(names, scores):
-    return None
+    highScoreInd = np.argmax(scores)
+    highScoreStu = names[highScoreInd]
+    return str(highScoreStu)
+
+lowest_score(names, scores)
