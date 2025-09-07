cd712b64bd46767b869f23fa3e559340101babf9
jgaughan2902 <jdgaugha@iu.edu>
2025-09-07 21:01:22 +0000

    Initial Commit

--- apputil.py
+++ apputil.py
@@ -1,10 +1,9 @@
+
 import numpy as np
 
 
-# update/add code below ...
-
-def ways(n):
-    return None
+def ways(n):                 # Define the ways function with n as the input.
+    return n // 5 + 1        # Return input n floor divided by 5 plus 1 because zero nickles is always an option so the answer will be
 
 def lowest_score(names, scores):
     return None
