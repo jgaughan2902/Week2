0cf6113d0bf8b675c4fafcd3b1c28a38ce53eeaa
jgaughan2902 <jdgaugha@iu.edu>
2025-09-08 02:08:09 +0000

    Making changes based on Week 1 feedback

--- apputil.py
+++ apputil.py
@@ -1,17 +1,60 @@
-def ways(n):                 # Define the ways function with n as the input.
-    return n // 5 + 1        # Return input n floor divided by 5 plus 1 because zero nickles is always an option so the answer will be
+def ways(n):
+    '''
+    Function to find the number of ways to 
+    make change for a given amount of cents (n).
 
-import numpy as np           # Import numpy for this question
+    Parameters:
+    n (int): The amount of cents you choose.
 
-def lowest_score(names, scores):                  # define the function with two inputs
-    lowScoreInd = np.argmin(scores)               # assign the minimum score index to lowScoreInd 
-    lowScoreStu = names[lowScoreInd]              # Find the name that matches the index
-    return lowScoreStu                            # Return the students name with the lowest score
+    Return value:
+    n // 5 + 1: An integer of the number of ways
+    to make change given n.
+    '''
+    return n // 5 + 1
+
+# Import numpy to use certain functions below
+import numpy as np
+
+def lowest_score(names, scores):
+    '''
+    Function to find the student with
+    the lowest score.
+
+    Parameters:
+    names (numpy.ndarray): The names of the students
+    scores (numpy.ndarray): The scores of those students
+
+    Return value:
+    lowScoreStu: The name of the student
+    with the lowest score
+    '''
+    # assign the minimum score index to lowScoreInd
+    lowScoreInd = np.argmin(scores)
+    # Find the name that matches the index
+    lowScoreStu = names[lowScoreInd]
+    return lowScoreStu
 
 
-def sort_names(names, scores):                    # define the function with the same two inputs as lowest_score
-    namesArray = np.array(names)                  # make names a numpy array (in order to make autograder work)
-    scoresArray = np.array(scores)                # make scores a numpy array (also in order to make autograder work)
-    sortedInd = np.argsort(scoresArray)[::-1]     # sort scores array indexes in descending order
-    sortedNames = namesArray[sortedInd]           # match the sorted scores indexes to corresponding names
-    return list(sortedNames)                      # return a list of the sorted names
+def sort_names(names, scores): 
+    '''
+    Function to list the names of the
+    students based on their score in
+    descending order.
+
+    Parameters:
+    names (numpy.ndarray): The names of the students
+    scores (numpy.ndarray): The scores of those students
+
+    Return value:
+    list(sortedNames): A list of the names
+    sorted in descending order based
+    on scores.
+    '''
+    # make names a numpy array (in order to make autograder work)
+    namesArray = np.array(names)
+    # sort scores array indexes in descending order
+    scoresArray = np.array(scores)                
+    sortedInd = np.argsort(scoresArray)[::-1]
+    # match the sorted scores indexes to corresponding names
+    sortedNames = namesArray[sortedInd]
+    return list(sortedNames)
