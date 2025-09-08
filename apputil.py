53837cde95f4e55d611d707f5b7db79ca0d46303
jgaughan2902 <jdgaugha@iu.edu>
2025-09-08 01:28:29 +0000

    Final changes including all comments

--- apputil.py
+++ apputil.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 def ways(n):                 # Define the ways function with n as the input.
     return n // 5 + 1        # Return input n floor divided by 5 plus 1 because zero nickles is always an option so the answer will be
 
